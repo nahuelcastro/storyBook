e3e37080b319ecc87cc04e6ab30a22eca69b69e4
Nahuel <>
2021-05-15 22:31:45 -0300

    db conectada, casi configurada toda la base

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 // Load config
@@ -10,9 +13,30 @@ connectDB()
 
 const app = express()
 
+//Logging
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+//Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs',}))
+app.set('view engine', '.hbs')
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Routes
+app.use('/', require('./routes/index'))
+
+
+
 const PORT = process.env.PORT || 3000
 
 app.listen(
     PORT,
     console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
     )
+
+// https://www.youtube.com/watch?v=SBvmnHTQIPY
+// min 30
